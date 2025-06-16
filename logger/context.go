cabbedb1912df67c9c9759c552ddfbc12dc40d3c
wangkun <w_wangkun@xiwang.com>
2025-06-16 15:33:03 +0800

    重构日志记录上下文处理，支持多种用户ID类型的记录；简化认证中间件，采用函数签名方式实现登录态检查。

--- logger/context.go
+++ logger/context.go
@@ -13,8 +13,15 @@ func FieldsFromCtx(ctx context.Context) []zap.Field {
 	if v, ok := ctx.Value(constant.CTX_TRACE_ID).(string); ok && v != "" {
 		fields = append(fields, zap.String(constant.CTX_TRACE_ID, v))
 	}
-	if v, ok := ctx.Value(constant.CTX_USER_ID).(string); ok && v != "" {
-		fields = append(fields, zap.String(constant.CTX_USER_ID, v))
+	if v := ctx.Value(constant.CTX_USER_ID); v != nil {
+		switch v := v.(type) {
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+			fields = append(fields, zap.Int(constant.CTX_USER_ID, v.(int)))
+		case string:
+			fields = append(fields, zap.String(constant.CTX_USER_ID, v))
+		default:
+			fields = append(fields, zap.Any(constant.CTX_USER_ID, v))
+		}
 	}
 	return fields
 }
